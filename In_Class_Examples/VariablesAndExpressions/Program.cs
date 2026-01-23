232e9726c559bbba8a14c760d191eee09c366bff
Ackerman, Adam P. <adam@ou.edu>
2026-01-23 11:52:38 -0600

    COmpleted in class example over variables and expressions

---
+++ In_Class_Examples/VariablesAndExpressions/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int num1 = 5;
+int num2;
+num2 = 10;
+
+double sum = num1 + num2;
+
+string name = "Adam";
+
+string message = "Hello  " + name + ", welcome to C# programming!";
+message = $"Hello {name}, welcome to C# programming!";
+Console.WriteLine("Hello, World!");
+Console.WriteLine(message);
