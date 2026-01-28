e835d5129130f5f32db4287c0b31612644772b6d
Ackerman, Adam P. <adam@ou.edu>
2026-01-28 11:50:29 -0600

    completed in class example

---
+++ In_Class_Examples/Conditional_Example2/Program.cs
@@ -0,0 +1,27 @@
+﻿Random r = new Random();
+
+int randomNumber = r.Next(1, 101);
+
+Console.WriteLine("Please enter a number <<");
+string input = Console.ReadLine();
+randomNumber = int.Parse(input); // Converts a string to an int
+randomNumber = Convert.ToInt32(input); // Another way to convert a string to an int
+
+Console.WriteLine(randomNumber);
+
+if (randomNumber % 3 == 0 && randomNumber % 5 == 0)
+{
+    Console.WriteLine("Fizzbuzz");
+}
+else if (randomNumber % 3 == 0)
+{
+    Console.WriteLine("Fizz");
+}
+else if (randomNumber % 5 == 0)
+{
+    Console.WriteLine("Buzz");
+}
+else
+{
+    Console.WriteLine(randomNumber);
+}
