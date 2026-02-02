ee6b804551d06011c57d95f5af212017f0a83ec1
Ackerman, Adam P. <adam@ou.edu>
2026-02-02 11:27:08 -0600

    completed participation over coin toss

---
+++ Participations/Conditional_Coin_Toss/Program.cs
@@ -0,0 +1,35 @@
+﻿//const int HEADS = 0;
+//const int TAILS = 1;
+const string DEVELOPER_INFO = "Developer: Professor Ackerman";
+
+Console.WriteLine("Welcome to my coin guessing game!");
+Random random = new Random();
+int tossAsInt = random.Next(0, 2); // 0 or 1
+
+//if (tossAsInt == HEADS)
+string toss;
+if (tossAsInt == 0)
+{
+    toss = "Heads";
+}
+else //if (tossAsInt == 1)
+{
+    toss = "Tails";
+}
+Console.WriteLine(toss); // For testing purposes, to see the result of the toss
+
+Console.WriteLine("Do you want to pick 'Heads' or 'Tails'? <<");
+string userGuess = Console.ReadLine();
+
+Console.WriteLine($"The coin landed on {toss}");
+
+if (userGuess.ToLower() == toss.ToLower())
+{
+    Console.WriteLine("You guessed correctly! You win!");
+}
+else
+{
+    Console.WriteLine("Sorry, your guess was incorrect. You lose.");
+}
+
+Console.WriteLine(DEVELOPER_INFO);
