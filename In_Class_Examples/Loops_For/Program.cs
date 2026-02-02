7dcfab4ff8ec793fb90315d6fa5f34c88cba63ff
Ackerman, Adam P. <adam@ou.edu>
2026-02-02 11:41:59 -0600

    Completed in class examples over loops

---
+++ In_Class_Examples/Loops_For/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Welcome to the Fizz Buzz Application");
+Console.WriteLine("What number do you want to calculate up to? <<");
+int upperBound = int.Parse(Console.ReadLine());
+
+
+for (int i = 1; i <= upperBound; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("Fizzbuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+
+}
---
+++ In_Class_Examples/Loops_While/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("What is your favorite number? <<");
+int favoriteNumber;
+string answer = Console.ReadLine();
+
+while (int.TryParse(answer, out favoriteNumber) == false)
+{
+    Console.WriteLine("Sorry that was not a number.  Please try again <<");
+    answer = Console.ReadLine();
+}
+
