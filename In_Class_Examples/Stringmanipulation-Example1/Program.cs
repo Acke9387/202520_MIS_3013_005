66f48d9f8fa07340ddba51b56eb30a6d69bfa91f
Ackerman, Adam P. <adam@ou.edu>
2026-02-11 11:46:22 -0600

    Completed in class example over string manipulation

---
+++ In_Class_Examples/Stringmanipulation-Example1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string sentence = "There's an art to getting your way, and spitting olive pits across the table isn't it.";
+
+int numberOfLetters = sentence.Length;
+char firstLetter = sentence[0];
+char lastLetter = sentence[numberOfLetters - 1];
+
+Console.WriteLine($"The first letter is {firstLetter} and the last letter is {lastLetter}");
+
+Console.WriteLine($"The sentence has {numberOfLetters} in it.");
+
+sentence = sentence.ToUpper();
+// Output every other letter of the sentence
+for (int i = 0; i < sentence.Length; i = i + 1)
+{
+    Console.WriteLine(sentence[i]);
+}
