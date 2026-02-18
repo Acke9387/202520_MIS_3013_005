d23d7217e2e6d732c67cc5877d71f92af11c0b8c
Ackerman, Adam P. <adam@ou.edu>
2026-02-18 11:48:18 -0600

    Completed in class examples over collections

---
+++ In_Class_Examples/Collections_Arrays/Program.cs
@@ -0,0 +1,52 @@
+﻿const int EXAM_COUNT = 3;
+double exam1;
+double exam2;
+double exam3;
+
+double[] exams = new double[EXAM_COUNT];
+int[] ids = new int[EXAM_COUNT];
+
+//Console.WriteLine("What is your score for exam 1? <<");
+//exam1 = double.Parse(Console.ReadLine());
+//Console.WriteLine("What is your score for exam 2? <<");
+//exam2 = double.Parse(Console.ReadLine());
+//Console.WriteLine("What is your score for exam 3? <<");
+//exam3 = double.Parse(Console.ReadLine());
+
+for (int i = 0; i < exams.Length; i++)
+{
+    Console.WriteLine($"What is your score for exam {i + 1}? <<");
+    exams[i] = double.Parse(Console.ReadLine());
+
+    Console.WriteLine($"What is the student's id who scored {exams[i]}? <<");
+    ids[i] = int.Parse(Console.ReadLine());
+}
+
+double sum = 0, average = 0;
+
+//for (int i = 0; i < exams.Length; i++)
+//{
+//    double grade = exams[i];
+//    sum += grade;
+
+//}
+foreach (double grade in exams)
+{
+    sum = sum + grade;
+}
+
+average = sum / exams.Length;
+
+Console.WriteLine($"The average of your {exams.Length} exams is {(average / 100).ToString("P")}");
+
+Console.WriteLine("Do you want to see everyones exam scores? yes or no <<");
+
+if (Console.ReadLine().ToLower() == "yes")
+{
+    Console.WriteLine("Student:\t\tExam Score:");
+    for (int i = 0; i < exams.Length; i++)
+    {
+        Console.WriteLine($"{ids[i]}\t\t\t{exams[i]}"); 
+    }
+
+}
---
+++ In_Class_Examples/Collections_Lists/Program.cs
@@ -0,0 +1,44 @@
+﻿
+List<double> exams = new List<double>();
+List<int> ids = new List<int>();
+int i = 0;
+do
+{
+    Console.WriteLine($"What is your score for exam {i + 1}? <<");
+    exams.Add(double.Parse(Console.ReadLine()));
+
+    Console.WriteLine($"What is the student's id who scored {exams[i]}? <<");
+    ids.Add(int.Parse(Console.ReadLine()));
+    i++;
+    Console.WriteLine("Do you have another students score to add? yes or no <<");
+} while (Console.ReadLine() == "yes");
+
+
+double sum = 0, average = 0;
+
+//for (int i = 0; i < exams.Length; i++)
+//{
+//    double grade = exams[i];
+//    sum += grade;
+
+//}
+foreach (double grade in exams)
+{
+    sum = sum + grade;
+}
+
+average = sum / exams.Count;
+
+Console.WriteLine($"The average of your {exams.Count} exams is {(average / 100).ToString("P")}");
+
+Console.WriteLine("Do you want to see everyones exam scores? yes or no <<");
+
+if (Console.ReadLine().ToLower() == "yes")
+{
+    Console.WriteLine("Student:\t\tExam Score:");
+    for (int x = 0; x < exams.Count; x++)
+    {
+        Console.WriteLine($"{ids[x]}\t\t\t{exams[x]}");
+    }
+
+}
