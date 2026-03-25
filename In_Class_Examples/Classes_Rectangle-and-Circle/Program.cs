c8d4b57270703271fd1f98d9cb5dbeb560ca14f7
Ackerman, Adam P. <adam@ou.edu>
2026-03-25 11:49:51 -0500

    Completed in class example over classes

---
+++ In_Class_Examples/Classes_Rectangle-and-Circle/Program.cs
@@ -0,0 +1,15 @@
+﻿using Classes_Rectangle_and_Circle;
+
+Rectangle r = new Rectangle();
+r.Length = 5;
+//r.SetWidth(3);
+r.Width = 10;
+
+Console.WriteLine(r.Width + " " + r.Length);
+
+
+Rectangle r2 = new Rectangle();
+r2.Length = 7;
+r2.Width = 38;
+
+Console.WriteLine(r2);
---
+++ In_Class_Examples/Classes_Rectangle-and-Circle/Rectangle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Rectangle_and_Circle
+{
+    public class Rectangle
+    {
+
+        /// <summary>
+        /// Gets or sets the length value.
+        /// </summary>
+        public double Length { get; set; }
+
+        public double Width { get; set; }
+
+
+        public double GetWidth()
+        {
+            return Width;
+        }
+
+        public void SetWidth(double width)
+        {
+            if (width < 0)
+            {
+                throw new ArgumentException("Width cannot be negative.");
+            }
+            Width = width;
+        }
+
+        public List<double> doubles { get; set; }
+        public Rectangle()
+        {
+            Length = 0;
+            Width = 0;
+            doubles = new List<double>();
+        }
+
+        public double CalculateArea()
+        {
+            double area = 0;
+            area = Width * Length;
+
+            return  area; 
+        }
+
+        /// <summary>
+        /// Calculates the perimeter of the rectangle based on its current length and width.
+        /// </summary>
+        /// <returns>The perimeter of the rectangle, calculated as twice the sum of its length and width.</returns>
+        public double CalculatePerimeter()
+        {
+            double perimeter = 0;
+
+            perimeter = 2 * (Length + Width);
+
+            return perimeter;
+        }
+
+        public override string ToString()
+        {
+            return $"The rectangle has a length of {Length} and a width of {Width}. The area is {CalculateArea()} and the perimeter is {CalculatePerimeter()}.";
+        }
+    }
+}
