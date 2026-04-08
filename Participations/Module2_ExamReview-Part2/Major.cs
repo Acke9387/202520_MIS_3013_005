b42a5c09f6462083991cb5626a2435cb92f95b54
Ackerman, Adam P. <adam@ou.edu>
2026-04-08 11:33:22 -0500

    Completed problem 2

---
+++ Participations/Module2_ExamReview-Part2/Major.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module2_ExamReview_Part2
+{
+    public class Major
+    {
+        public string MajorName { get; set; }
+        public double AverageSalary { get; set; }
+        public double RequriedGPA { get; set; }
+        public double GraduationRate { get; set; }
+        public double AverageStudentDebt { get; set; }
+        public int NumberOfStudents { get; set; }
+        public int RequiredCourses { get; set; }
+
+        public Major()
+        {
+            MajorName = string.Empty;
+            AverageSalary = 0;
+            RequriedGPA = 0;
+            GraduationRate = 0;
+            AverageStudentDebt = 0;
+            NumberOfStudents = 0;
+            RequiredCourses = 0;
+        }
+
+        public Major(double salary) : this()
+        {
+            AverageSalary = salary;
+            //MajorName = string.Empty;
+            //RequriedGPA = 0;
+            //GraduationRate = 0;
+            //AverageStudentDebt = 0;
+            //NumberOfStudents = 0;
+            //RequiredCourses = 0;
+        }
+        public Major(double salary, string name) : this()
+        {
+            AverageSalary = salary;
+            MajorName = name;
+            //RequriedGPA = 0;
+            //GraduationRate = 0;
+            //AverageStudentDebt = 0;
+            //NumberOfStudents = 0;
+            //RequiredCourses = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{MajorName} has a required gpa of {RequriedGPA.ToString("N")}";
+        }
+
+    }
+}
--- Participations/Module2_ExamReview-Part2/Program.cs
+++ Participations/Module2_ExamReview-Part2/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Module2_ExamReview_Part2;
+
+List<Major> majors = new List<Major>();
+
+do
+{
+    Major m = new Major();
+    //MajorName = string.Empty;
+    //AverageSalary = 0;
+    //RequriedGPA = 0;
+    //GraduationRate = 0;
+    //AverageStudentDebt = 0;
+    //NumberOfStudents = 0;
+    //RequiredCourses = 0;
+
+    Console.WriteLine("What is the majors name? <<");
+    string name = Console.ReadLine();
+    m.MajorName = name;
+    //m.MajorName = Console.ReadLine();
+    Console.WriteLine("What is the average salary? <<");
+    m.AverageSalary = double.Parse(Console.ReadLine());
+    Console.WriteLine("What is the required GPA? <<");
+    m.RequriedGPA = double.Parse(Console.ReadLine());
+    Console.WriteLine("What is the graduation rate?<<");
+    m.GraduationRate = double.Parse(Console.ReadLine());
+    Console.WriteLine("What is the average student debt?");
+    m.AverageStudentDebt = double.Parse(Console.ReadLine());
+    Console.WriteLine("WHat is the number of students?");
+    m.NumberOfStudents = int.Parse(Console.ReadLine());
+    Console.WriteLine("How many required courses are there? <<");
+    m.RequiredCourses = int.Parse(Console.ReadLine());
+
+    majors.Add(m);
+
+    Console.WriteLine("Do you have another major to enter? yes or no <<");
+} while (Console.ReadLine().ToLower() == "yes");
+
+Console.WriteLine("What is the minimum salary of a Major that you want to see in your results?<<");
+double minSalary = double.Parse(Console.ReadLine());
+
+foreach (Major m in majors)
+{
+    if (m.AverageSalary >= minSalary)
+    {
+        Console.WriteLine(m);
+    }
+}
