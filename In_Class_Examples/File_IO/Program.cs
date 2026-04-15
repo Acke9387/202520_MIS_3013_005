e35decad86deb22174395d3f93600d6f58b90eab
Ackerman, Adam P. <adam@ou.edu>
2026-04-15 11:26:50 -0500

    Started in class example

---
+++ In_Class_Examples/File_IO/Program.cs
@@ -0,0 +1,8 @@
+﻿
+string[] fileContents = File.ReadAllLines("students.csv");
+
+for(int i = 1; i < fileContents.Length; i++)
+{
+    string line = fileContents[i];
+    Console.WriteLine(line);
+}
---
+++ In_Class_Examples/File_IO/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace File_IO
+{
+    public class Student
+    {
+        //full_name,gender,birthdate,email,city
+        public string full_name { get; set; }
+        public string gender { get; set; }
+        public DateTime birthdate { get; set; }
+        public string email { get; set; }
+        public string city { get; set; }
+
+        public Student()
+        {
+            full_name = string.Empty;
+            gender = string.Empty;
+            birthdate = DateTime.MinValue;
+            email = string.Empty;
+            city = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{full_name} was born on {birthdate.ToShortDateString()} and lives in {city}. You can contact them at {email}.";
+        }
+
+    }
+}
