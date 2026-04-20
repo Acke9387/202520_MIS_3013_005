d06912ac67b9d49ce4003ef03713e338e012a459
Ackerman, Adam P. <adam@ou.edu>
2026-04-20 11:11:13 -0500

    Completed partcipiation over cereal

---
+++ Participations/Classes_Cereal/Cereal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Cereal
+{
+    public class Cereal
+    {
+
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Calories { get; set; }
+        public double Cups { get; set; }
+
+        public Cereal()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Calories = 0;
+            Cups = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} by {Manufacturer} has {Calories} calories and comes in {Cups} cups.";
+        }
+
+
+    }
+}
---
+++ Participations/Classes_Cereal/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using Classes_Cereal;
+
+string[] linesOfFile = File.ReadAllLines("Cereal_Data.txt");
+List<Cereal> cereals = new List<Cereal>();
+
+
+for(int i = 1; i < linesOfFile.Length; i++)
+{
+    string[] piecesOfLine = linesOfFile[i].Split('|');
+    // 0        1          2      3
+    //name|manufacturer|calories|cups
+
+    Cereal temp = new Cereal();
+    temp.Name = piecesOfLine[0];
+    temp.Manufacturer = piecesOfLine[1];
+    temp.Calories = int.Parse(piecesOfLine[2]);
+    temp.Cups = double.Parse(piecesOfLine[3]);
+
+    cereals.Add(temp);
+}
+
+
+//output all of the Cereal information that have a serving size that is 1 cup or more.
+Console.WriteLine("Cereals with a serving size of 1 cup or more:");
+foreach (Cereal cereal in cereals)
+{
+    if (cereal.Cups >= 1)
+    {
+        Console.WriteLine($"\t{cereal}");
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Cereals with 100 calories or less per serving:");
+foreach (Cereal cereal in cereals)
+{
+    if (cereal.Calories <= 100)
+    {
+        Console.WriteLine($"\t{cereal}");
+    }
+}
