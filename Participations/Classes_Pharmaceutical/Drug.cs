f5e6b546636afd72ec2d5a94f6e1ee540c1f2ecb
Ackerman, Adam P. <adam@ou.edu>
2026-04-24 11:09:17 -0500

    Completed participation over pharmaceutical drugs

---
+++ Participations/Classes_Pharmaceutical/Drug.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Pharmaceutical
+{
+    public class Drug
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Brand { get; set; }
+        public int Code { get; set; }
+        public string StreetName { get; set; }
+
+        public Drug()
+        {
+            Name = string.Empty;
+            Company = string.Empty;
+            Brand = string.Empty;
+            Code = 0;
+            StreetName = string.Empty;
+        }
+        public override string ToString()
+        {
+            return $"{StreetName} is made by {Company} and has a code of {Code}.";
+        }
+    }
+}
--- Participations/Classes_Pharmaceutical/Program.cs
+++ Participations/Classes_Pharmaceutical/Program.cs
@@ -1,1 +1,46 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Classes_Pharmaceutical;
+
+List<Drug> drugList = new List<Drug>();
+string[] linesOfFile = File.ReadAllLines("Pharmaceutical Drugs.csv");
+
+for (int i = 1; i < linesOfFile.Length; i++)
+{
+    Drug drug = new Drug();
+    // 0     1      2     3      4
+    //Name,Company,Brand,Code,StreetName
+    string[] parts = linesOfFile[i].Split(',');
+    drug.Name = parts[0];
+    drug.Company = parts[1];
+    drug.Brand = parts[2];
+    drug.Code = int.Parse(parts[3]);
+    drug.StreetName = parts[4];
+
+    drugList.Add(drug);
+}
+
+DisplayAllDrugs(drugList);
+Console.WriteLine("Which drug would you like to see all that match?");
+string input = Console.ReadLine();
+Console.Clear();
+
+foreach (var d in drugList)
+{
+    if (d.Name == input)
+    {
+        Console.WriteLine(d);
+    }
+}
+
+void DisplayAllDrugs(List<Drug> drugList)
+{
+    List<string> names = new List<string>();
+
+    foreach (var d in drugList)
+    {
+        if (names.Contains(d.Name) == false)
+        {
+            Console.WriteLine(d.Name);
+            names.Add(d.Name);
+        }
+    }
+}
