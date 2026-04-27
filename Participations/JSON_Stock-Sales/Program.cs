bc0000abebb73033a735b1d69c4e3a4add9f349d
Ackerman, Adam P. <adam@ou.edu>
2026-04-27 11:17:05 -0500

    started participation

---
+++ Participations/JSON_Stock-Sales/Program.cs
@@ -0,0 +1,6 @@
+﻿using JSON_Stock_Sales;
+
+List<StockOption> stocks = new List<StockOption>();
+string fileContents = File.ReadAllText("stock_sales.json");
+
+
---
+++ Participations/JSON_Stock-Sales/StockOption.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_Stock_Sales
+{
+    public class StockOption
+    {
+
+        public object BuyerName { get; set; }
+        public object PurchasePrice { get; set; }
+        public object SalePrice { get; set; }
+        public object StockSymbol { get; set; }
+        public object PurchaseDate { get; set; }
+        public object SaleDate { get; set; }
+
+
+        public StockOption()
+        {
+
+        }
+
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
