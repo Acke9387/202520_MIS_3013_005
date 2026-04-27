c2dce5edb33b63597efc8cac41660b8d0e3c7d5d
Ackerman, Adam P. <adam@ou.edu>
2026-04-27 11:08:01 -0500

    completed participation over JSON from a file

---
+++ Participations/JSON_From-A-File/Owner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_From_A_File
+{
+    public class Owner
+    {
+
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Make { get; set; }
+        public int Year { get; set; }
+        public String Model { get; set; }
+        public String Color { get; set; }
+
+        public Owner()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Make = string.Empty;
+            Year = 0;
+            Model = string.Empty;
+            Color = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            // display all information
+            return $"{FirstName} {LastName} owns a {Make} {Model} that is {Color} made in {Year}";
+        }
+
+    }
+}
---
+++ Participations/JSON_From-A-File/Program.cs
@@ -0,0 +1,38 @@
+﻿using JSON_From_A_File;
+using Newtonsoft.Json;
+
+List<Owner> owners = new List<Owner>();
+string fileContents = File.ReadAllText("Car_Owners.json");
+
+owners = JsonConvert.DeserializeObject<List<Owner>>(fileContents);
+
+string answer = "No";
+while (answer.ToLower() == "no")
+{
+    Console.WriteLine("Here are your options:\n1)Display all information from owners\n2)Display owner information who own a car with a particular color.\nWhich option do you choose either 1 or 2? <<");
+    int option = int.Parse(Console.ReadLine());
+    switch (option)
+    {
+        case 1: //All info of owners
+            foreach (var item in owners)
+            {
+                Console.WriteLine(item);
+            }
+            break;
+        case 2: //All info based on color
+            Console.WriteLine("What color do you want to see a car from? <<");
+            answer = Console.ReadLine();
+            foreach (var item in owners)
+            {
+                if (item.Color == answer)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            break;
+        default:
+            break;
+    }
+
+    Console.WriteLine("Do you want to exit the application? yes or no <<");
+}
