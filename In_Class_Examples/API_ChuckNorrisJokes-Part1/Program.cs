22d33d0d983a93efc8abf50157e22f19ef22f743
Ackerman, Adam P. <adam@ou.edu>
2026-05-08 11:20:02 -0500

    completed part 2 of participation

--- In_Class_Examples/API_ChuckNorrisJokes-Part1/Program.cs
+++ In_Class_Examples/API_ChuckNorrisJokes-Part1/Program.cs
@@ -1,11 +1,45 @@
 ﻿using API_ChuckNorrisJokes_Part1;
 using Newtonsoft.Json;
 
-string url = "https://api.chucknorris.io/jokes/random";
 ChuckNorrisAPI joke = new ChuckNorrisAPI();
+List<string> categories = new List<string>();
+List<ChuckNorrisAPI> jokes = new List<ChuckNorrisAPI>();
+
+using (var client = new HttpClient())
+{
+    var response = await client.GetAsync("https://api.chucknorris.io/jokes/categories");
+    if (response.IsSuccessStatusCode)
+    {
+        string json = await response.Content.ReadAsStringAsync();
+        categories = JsonConvert.DeserializeObject<List<string>>(json);
+    }
+    else
+    {
+        Console.WriteLine("Failed to retrieve categories.");
+        return;
+    }
+}
+
 
 do
 {
+    Console.Clear();
+    Console.WriteLine("Available categories:");
+    Console.WriteLine($"- all");
+    foreach (var c in categories)
+    {
+        Console.WriteLine($"- {c}");
+    }
+
+    Console.WriteLine("Please select a category:");
+    string category = Console.ReadLine();
+
+    string url = $"https://api.chucknorris.io/jokes/random";
+    if (category.ToLower() != "all")
+    {
+        url += $"?category={category}";
+    }
+
     using (var client = new HttpClient())
     {
         var response = await client.GetAsync(url);
@@ -14,6 +48,7 @@ do
             string json = await response.Content.ReadAsStringAsync();
             joke = JsonConvert.DeserializeObject<ChuckNorrisAPI>(json);
             Console.WriteLine(joke);
+            jokes.Add(joke);
         }
         else
         {
@@ -25,3 +60,8 @@ do
 
     Console.WriteLine("Do you want another joke? y/n <<");
 } while (Console.ReadLine().ToLower() == "y");
+
+string fileName = "jokes.json";
+string fileContents = JsonConvert.SerializeObject(jokes);
+File.WriteAllText(fileName, fileContents);
+Console.WriteLine($"Saved all jokes to {fileName}.");
